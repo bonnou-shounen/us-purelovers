59f5394329d412676441f751553495c7a3173b44
煩悩少年 <bonnou.shounen@gmail.com>
2022-06-26 22:34:30 +0900

    fix link to official

--- purelovers-plus.user.js
+++ purelovers-plus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        purelovers-plus
-// @version     0.1.5
+// @version     0.1.6
 // @match       https://purelovers.com/*
 // ==/UserScript==
 
@@ -24,8 +24,8 @@
         divsroot.insertBefore(schediv, divsroot.children[2])
     }
 
-    // 店名をオフィシャルサイトへのリンクにする
-    const sn = document.querySelector('.k_globalInforShop h1')
+    // 可能ならオフィシャルサイトへのリンクをつける
+    const sn = document.querySelector('.k_globalInforShop-body h1')
     if (sn) {
         const table = {
             568: 'https://www.seyten.com/schedule/',
@@ -44,7 +44,12 @@
         }
         const os = table[location.pathname.match(/\/shop\/(\d+)/)[1] - 0]
         if (os) {
-            sn.innerHTML = '<a href="' + os + '" target="_blank" class="k_pink--text k_text-hover--blue-accent-4">' + sn.innerHTML + '</a>'
+            sn.innerHTML = `${sn.innerHTML} &nbsp;/&nbsp;
+              <a href="${os}" target="_blank" class="k_text k_grey--text k_text--darken-3 k_text--large k_text-dense k_text-height--default k_text-inline-block">
+                 <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="18px" height="18px">
+                   <path d="M 19.980469 2.9902344 A 1.0001 1.0001 0 0 0 19.869141 3 L 15 3 A 1.0001 1.0001 0 1 0 15 5 L 17.585938 5 L 8.2929688 14.292969 A 1.0001 1.0001 0 1 0 9.7070312 15.707031 L 19 6.4140625 L 19 9 A 1.0001 1.0001 0 1 0 21 9 L 21 4.1269531 A 1.0001 1.0001 0 0 0 19.980469 2.9902344 z M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 13 A 1.0001 1.0001 0 1 0 19 13 L 19 19 L 5 19 L 5 5 L 11 5 A 1.0001 1.0001 0 1 0 11 3 L 5 3 z"/>
+                 </svg>
+            </a>`
         }
     }
 
